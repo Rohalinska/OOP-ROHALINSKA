c2e54abbb4a5f1d5e192ce854d077581ebd08b84
Oleksandra Rohalinska <oleksandrarogalinska71@gmail.com>
2026-01-21 20:31:19 +0200

    lab21v4

---
+++ OOP-ROHALINSKA/lab21v4/NightShippingStrategy.cs
@@ -0,0 +1,11 @@
+namespace lab21v4.Strategies
+{
+    public class NightShippingStrategy : IShippingStrategy
+    {
+        public decimal CalculateCost(decimal distance, decimal weight)
+        {
+            decimal standardCost = distance * 1.5m + weight * 0.5m;
+            return standardCost + 30; 
+        }
+    }
+}
